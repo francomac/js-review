11eaed086d992dd621a41d64c0d27faf3f52bc58
Franco Martinez Carballo <francomac@Francos-MacBook-Pro.local>
2024-03-01 16:10:58 -0600

    arrow functions, recursion, .some, .find

--- src/index.js
+++ src/index.js
@@ -3,19 +3,59 @@ console.log(
   "background: green; color: white; display: block;"
 );
 
-const notCommonArray = new Array(100); // its not a definition very common
+const saludar = (nombre) => `Hola mundo ${nombre}`; // arrow or lambda function
 
-const commonArray = [1, 2, 3];
-commonArray.push(5); // add to end
+const nombre = "franco";
 
-const anotherCommonArray = [...commonArray]; // spread operator
-anotherCommonArray.push(6);
+console.log(saludar(nombre));
 
-const mappedArray = anotherCommonArray.map((n) => {
-  return n * 2;
+const heroes = [
+  {
+    id: 1,
+    name: "Batman",
+  },
+  {
+    id: 2,
+    name: "El Chavo",
+  },
+  {
+    id: 3,
+    name: "Superman",
+  },
+];
+
+// option #1 using recursion
+let heroIndx = 0;
+const heroeExiste = (name) => {
+  if (name === heroes[heroIndx].name) {
+    heroIndx = 0;
+    return `${name} sí existe!`;
+  } else {
+    heroIndx = heroIndx < heroes.length - 1 ? heroIndx + 1 : 0;
+
+    if (heroIndx === 0) {
+      return `${name} NO existe!`;
+    }
+
+    return heroeExiste(name);
+  }
+};
+
+console.log(heroeExiste("Superman"));
+console.log(heroeExiste("El Chavon"));
+console.log(heroeExiste("Batman"));
+console.log(heroeExiste("Batiman"));
+
+// option #2 using some
+
+const existeSome = heroes.some((h) => {
+  return h.id === 1;
 });
-mappedArray.push(14);
+console.log(existeSome);
 
-console.log("commonArray", commonArray);
-console.log("anotherCommonArray", anotherCommonArray);
-console.log("mappedArray", mappedArray);
+// option #3 using find
+
+const existeFind = heroes.find((h) => {
+  return h.id === 1;
+});
+console.log(existeFind.name);
