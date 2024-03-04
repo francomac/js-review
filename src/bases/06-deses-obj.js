da5b53820b46be920faa89173b70c73071921789
francomac <franco.mcarballo@outlook.com>
2024-03-04 11:02:29 -0600

    destructuring objects

--- src/bases/06-deses-obj.js
+++ src/bases/06-deses-obj.js
@@ -1,29 +1,29 @@
-
-
 const person = {
-    name: 'Tony',
-    age: 45,
-    codeName: 'Ironman',
-}
+  name: "franco",
+  age: 45,
+  codeName: "francomac",
+  power: "money",
+};
 
-const { age, name, codeName, power = 'No tiene poder' } = person
+const { name, age, codeName, power = "No Power" } = person;
 
-// console.log( name )
-// console.log( age )
-// console.log( codeName )
-// console.log( power )
+console.log("%cindex.js line:14 name", "color: #007acc;", name);
+console.log("%cindex.js line:14 age", "color: #007acc;", age);
+console.log("%cindex.js line:14 codeName", "color: #007acc;", codeName);
+console.log("%cindex.js line:14 power", "color: #007acc;", power); // comment this out
 
-const createHero = ({ name, age, codeName, power }) => 
-    ({
-        id: 1123416523,
-        name,
-        age,
-        codeName,
-        power,
-    })
+// =================================================
 
+const createHero = ({ name, age, codeName, power = "No Power" }) => ({
+  id: 123434234,
+  name,
+  age,
+  codeName,
+  power,
+});
 
-console.log(  createHero( person )   )
-
-
-
+console.log(
+  "%cindex.js line:29 createHero()",
+  "color: #007acc;",
+  createHero(person)
+);
