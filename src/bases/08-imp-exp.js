613ffb3420f2ec1578ffae388fac40db4bcd9316
francomac <franco.mcarballo@outlook.com>
2024-03-04 11:48:56 -0600

    imports and exports

--- src/bases/08-imp-exp.js
+++ src/bases/08-imp-exp.js
@@ -1,53 +1,9 @@
-
 // import { owners } from './data/heroes'
-import superHeroes from '../data/heroes'
+import superHeroes from "../data/heroes";
 
+// ==============================================
+export const getHeroById = (id) => superHeroes.find((h) => h.id === id);
 
-// getHeroById ( id ) 
-// funciones de Flecha
-
-// const getHeroById = ( id ) => {
-//     return superHeroes.find( hero => hero.id === id )
-// }
-export const getHeroById = ( id ) => superHeroes.find( hero => hero.id === id )
-
-
-// getHeroesByOwner 'DC', 'Marvel'
-export const getHeroesByOwner = ( owner ) => superHeroes.filter( hero => hero.owner === owner )
-
-
-
-
-
-
-
-// import { getHeroById, getHeroesByOwner } from './bases/08-imp-exp'
-
-
-// console.log( getHeroById(2) ) // Spiderman
-
-// console.log( getHeroesByOwner('Marvel') ) // []
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ==============================================
+export const getHeroByOwner = (owner) =>
+  superHeroes.filter((h) => h.owner === owner);
--- src/index.js
+++ src/index.js
@@ -2,3 +2,19 @@ console.log(
   "%cHello index.js line:1 ",
   "background: green; color: white; display: block;"
 );
+
+// ==============================================
+
+import { getHeroByOwner, getHeroById } from "./bases/08-imp-exp";
+
+console.log(
+  "%cindex.js line:20 getHeroById",
+  "color: #007acc;",
+  getHeroById(2)
+);
+
+console.log(
+  "%cindex.js line:20 getHeroById",
+  "color: #007acc;",
+  getHeroByOwner("Marvel")
+);
