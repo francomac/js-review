1ffb5e919578323cf1f7ab2827f30429d2b0de2f
francomac <franco.mcarballo@outlook.com>
2024-03-05 15:01:39 -0600

    fetch

--- src/bases/10-fetch.js
+++ src/bases/10-fetch.js
@@ -1,9 +1,9 @@
 const apiKey = "3VRPtuzTPBydcxV9vypiSQ5g7mf0YUYd";
-const url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`;
+const url = `https://api.giphy.com/v1/gifs/random/trending?api_key=${apiKey}`;
 fetch(url)
   .then((res) => res.json()) // destructuring to get data only, on line 10
   .then(({ data }) => {
-    const { url } = data[45].images.original;
+    const { url } = data.images.original;
 
     const img = document.createElement("img");
     img.src = url;
