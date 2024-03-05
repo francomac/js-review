c06d53ec6e4d151890f760a1d225c4cdd2f0afef
francomac <franco.mcarballo@outlook.com>
2024-03-05 15:01:29 -0600

    axios

--- src/index.js
+++ src/index.js
@@ -2,3 +2,24 @@ console.log(
   "%cHola Mundo line:1 ",
   "background: green; color: white; display: block;"
 );
+
+import axios from "axios";
+
+const apiKey = "3VRPtuzTPBydcxV9vypiSQ5g7mf0YUYd";
+// const url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`;
+
+const giphyApi = axios.create({
+  baseURL: "https://api.giphy.com/v1/gifs",
+  params: {
+    api_key: apiKey,
+  },
+});
+
+giphyApi.get("/random").then((res) => {
+  const { data } = res.data;
+  const { url } = data.images.original;
+
+  const img = document.createElement("img");
+  img.src = url;
+  document.body.append(img);
+});
